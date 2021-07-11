56dff361339b04f93e7b2cfc34352438243bb0e0
Irina Ulianova <ulianovira.89@gmail.com>
2021-07-11 13:24:27 +0300

    add modalwindow

--- app.js
+++ app.js
@@ -63,3 +63,79 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+/*Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+Открытие модального окна по клику на элементе галереи.
+Подмена значения атрибута src элемента img.lightbox__image.
+Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для 
+того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.*/
+// console.log(galleryItems);
+// const boxImageEl = document.querySelector('.js-gallery');
+
+// console.log(boxImageEl);
+
+// // const closeBtn = document.querySelector('[data-action="close-lightbox"]');
+// // const modalWindowEl = document.querySelector('.js-lightbox');
+
+// //  Добавили разметку
+// function createImageGallery(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `<li class = "gallery__item"><a class = "gallery__link" >
+//     <img class = "gallery__image" src = ${preview} data-source = ${preview} alt = ${description}/></a></li>`;
+//     })
+//     .join('');
+// }
+
+// // const modalWindowImgEl = document.querySelector('.lightbox__image');
+// const cardsGallery = createImageGallery(galleryItems);
+// boxImageEl.insertAdjacentHTML(`beforeend`, createImageGallery);
+const boxImageEl = document.querySelector('.js-gallery');
+const closeBtn = document.querySelector('[data-action="close-lightbox"]');
+const modalWindowEl = document.querySelector('.js-lightbox');
+const modalWindowImgEl = document.querySelector('.lightbox__image');
+// Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+const createImageGallery = array => {
+  return array
+    .map(({ preview }) => {
+      return `
+        <li class="gallery__item">
+        <img class="gallery__image" src="${preview} ">
+        </li>
+        `;
+    })
+    .join('');
+};
+
+const cardsGallery = createImageGallery(galleryItems);
+
+boxImageEl.insertAdjacentHTML('beforeend', cardsGallery);
+
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+boxImageEl.addEventListener('click', onCardsGalleryClick);
+closeBtn.addEventListener('click', onCloseBtn);
+// Открытие модального окна по клику на элементе галереи.
+// Подмена значения атрибута src элемента img.lightbox__image.
+function onCardsGalleryClick(event) {
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  modalWindowEl.classList.add('is-open');
+
+  const findImgHref = array => {
+    return array
+      .map(item => {
+        if (item.preview === event.target.src) {
+          return item.original;
+        }
+      })
+      .join('');
+  };
+  modalWindowImgEl.setAttribute('src', findImgHref(galleryItems));
+}
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+function onCloseBtn(event) {
+  modalWindowImgEl.setAttribute('src', '');
+  modalWindowEl.classList.remove('is-open');
+}
